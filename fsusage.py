04cf24057f88998488252dca4f8baf821fe499c1
Cyrus Daboo <cdaboo@apple.com>
2007-02-09 21:47:34 +0000

    Fix comment. Allow loop over several scripts and produce tabular output.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalDAVTester/trunk@1157 e27351fd-9f3e-4f54-a53b-843176b1656c

--- fsusage.py
+++ fsusage.py
@@ -18,9 +18,9 @@
 # DRI: Cyrus Daboo, cdaboo@apple.com
 ##
 #
-# Creates some test accounts on an OpenDirectory server for use with CalDAVTester
+# Generates fs_usage counts for CalDAVTester script runs.
 #
-import time
+
 import subprocess
 
 from src.manager import manager
@@ -35,6 +35,26 @@ Options:
     -h       Print this help and exit
     -f file  pid file of caldavd server
 """
+
+def getFSUsage(testscript, runs, pid):
+    fd = open("temp", "w")
+    cid = subprocess.Popen(["fs_usage", "-f", "filesys", "%s" %(pid,),], stdout=fd).pid
+    
+    sname = "scripts/server/serverinfo.xml"
+    pname = None
+    fnames = [testscript]
+
+    mgr = manager(level=manager.LOG_NONE)
+    result, timing = mgr.runWithOptions(sname, pname, fnames, {})
+
+    os.kill(cid, signal.SIGTERM)
+
+    fd = open("temp", "r")
+    ctr = 0
+    for line in fd:
+        ctr += 1
+
+    return ctr / runs
 
 if __name__ == "__main__":
 
@@ -57,16 +77,16 @@ if __name__ == "__main__":
     pid = int(s)
     fd = None
     
-    fd = open("temp", "w")
-    cid = subprocess.Popen(["fs_usage", "-f", "filesys", "%s" %(pid,),], ).pid
+    tests = (
+        ("performance/get/get-small.xml", 10, "GET small",),
+        ("performance/get/get-large.xml", 10, "GET large",),
+        ("performance/put/put-small.xml", 10, "PUT small",),
+        ("performance/put/put-large.xml", 10, "PUT large",),
+        ("performance/propfind/propfind-small.xml", 10, "PROPFIND small",),
+        ("performance/propfind/propfind-medium.xml", 10, "PROPFIND medium",),
+        ("performance/propfind/propfind-large.xml", 10, "PROPFIND large",),
+    )
     
-    sname = "scripts/server/serverinfo.xml"
-    pname = None
-    fnames = ["performance/propfind/propfind-large.xml"]
-
-    mgr = manager(level=manager.LOG_NONE)
-    result, timing = mgr.runWithOptions(sname, pname, fnames, {})
-
-    print cid
-    #time.sleep(5)
-    os.kill(cid, signal.SIGTERM)
+    result = []
+    for test in tests:
+        print "%s\t%s" % (test[2], getFSUsage(test[0], test[1], pid),)
