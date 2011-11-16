apple/ccs-caldavtester

commit d8ef4b572c01ef6fc0160fb134acd00aa6d58a3f

Define AugmenetService in caldavd.plist, move patchConfig up a bit to happen before resource creation
git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalDAVTester/trunk@8218 e27351fd-9f3e-4f54-a53b-843176b1656c

odsetup.py
@@ -326,7 +326,14 @@ def readConfig(config):
 
 def patchConfig(config, admin):
     """
-    Patch the caldavd.plist file to make sure the proper admin principal is configured.
+    Patch the caldavd.plist file to make sure:
+       * the proper admin principal is configured
+       * DS Search node is set to /LDAPv3/127.0.0.1
+       * iMIP is disabled
+       * SACLs are disabled
+       * CalDAV and CardDAV are enabled
+       * EnableAnonymousReadRoot is enabled
+       * AugmentService is configured
 
     @param config: file path to caldavd.plist
     @type config: str
@@ -353,6 +360,7 @@ def patchConfig(config, admin):
 
     # Needed for CDT
     plist["EnableAnonymousReadRoot"] = True
+    plist["AugmentService"] = {'params': {'xmlFiles': ['augments.xml']}, 'type': 'twistedcaldav.directory.augment.AugmentXMLDB'}
 
     writePlist(plist, config)
 
@@ -692,6 +700,9 @@ if __name__ == "__main__":
             # Patch the sudoers file for the superuser principal.
             patchSudoers(sudoers)
     
+            # Patch the caldavd.plist file with the testadmin user's guid-based principal-URL
+            patchConfig(config, "/principals/__uids__/%s/" % (guids["testadmin"],))
+
             # Now generate the OD accounts (caching guids as we go).
             if protocol == "caldav":
                 loadLists(config, "/Places", locations)
@@ -699,9 +710,6 @@ if __name__ == "__main__":
 
             doToAccounts(config, protocol, createUser)
             doGroupMemberships()
-            
-            # Patch the caldavd.plist file with the testadmin user's guid-based principal-URL
-            patchConfig(config, "/principals/__uids__/%s/" % (guids["testadmin"],))
             
             # Create an appropriate serverinfo.xml file from the template
             buildServerinfo(serverinfo_default, hostname, port, sslport, authtype, docroot)

commit 01937f6f9b5401d567b1d286e0dd8b7d904822a2

Fix bug introduced in r8218; patchConfig has to happen after principal records are created. Configuring AugmentService has to happen before records are created. 10399622
git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalDAVTester/trunk@8291 e27351fd-9f3e-4f54-a53b-843176b1656c

odsetup.py
@@ -333,7 +333,6 @@ def patchConfig(config, admin):
        * SACLs are disabled
        * CalDAV and CardDAV are enabled
        * EnableAnonymousReadRoot is enabled
-       * AugmentService is configured
 
     @param config: file path to caldavd.plist
     @type config: str
@@ -360,8 +359,18 @@ def patchConfig(config, admin):
 
     # Needed for CDT
     plist["EnableAnonymousReadRoot"] = True
-    plist["AugmentService"] = {'params': {'xmlFiles': ['augments.xml']}, 'type': 'twistedcaldav.directory.augment.AugmentXMLDB'}
 
+    writePlist(plist, config)
+
+def patchConfigForAugmentService(config):
+    """
+    Patch the caldavd.plist file to configure AugmentService
+
+    @param config: file path to caldavd.plist
+    @type config: str
+    """
+    plist = readPlist(config)
+    plist["AugmentService"] = {'params': {'xmlFiles': ['augments.xml']}, 'type': 'twistedcaldav.directory.augment.AugmentXMLDB'}
     writePlist(plist, config)
 
 def patchSudoers(sudoers):
@@ -700,8 +709,8 @@ if __name__ == "__main__":
             # Patch the sudoers file for the superuser principal.
             patchSudoers(sudoers)
     
-            # Patch the caldavd.plist file with the testadmin user's guid-based principal-URL
-            patchConfig(config, "/principals/__uids__/%s/" % (guids["testadmin"],))
+            # Patch caldavd.plist to configure AugmentService, needed before creating resources
+            patchConfigForAugmentService(config)
 
             # Now generate the OD accounts (caching guids as we go).
             if protocol == "caldav":
@@ -711,6 +720,9 @@ if __name__ == "__main__":
             doToAccounts(config, protocol, createUser)
             doGroupMemberships()
             
+            # Patch the caldavd.plist file with the testadmin user's guid-based principal-URL
+            patchConfig(config, "/principals/__uids__/%s/" % (guids["testadmin"],))
+
             # Create an appropriate serverinfo.xml file from the template
             buildServerinfo(serverinfo_default, hostname, port, sslport, authtype, docroot)
 