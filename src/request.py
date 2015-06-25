48f43c006196828e02aa6b3dc709899a9d873e2f
Cyrus Daboo <cdaboo@apple.com>
2015-06-25 21:56:18 +0000

    Fix digest nonce timeout. Make sure digest OPTIONS request uri is properly quoted.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/CalDAVTester/trunk@14917 e27351fd-9f3e-4f54-a53b-843176b1656c

--- src/request.py
+++ src/request.py
@@ -27,6 +27,8 @@ import os
 import re
 import src.xmlDefs
 import time
+import urllib
+import urlparse
 import uuid
 
 algorithms = {
@@ -228,16 +230,20 @@ class request(object):
 
     def gethttpdigestauth(self, si, wwwauthorize=None):
 
-        # Check the nonce cache to see if we've used this user before
+        # Check the nonce cache to see if we've used this user before, or if the nonce is more than 5 minutes old
         user = [self.user, si.user][self.user == ""]
         pswd = [self.pswd, si.pswd][self.pswd == ""]
         details = None
-        if user in self.manager.digestCache:
+        if user in self.manager.digestCache and self.manager.digestCache[user]["max-nonce-time"] < time.time():
             details = self.manager.digestCache[user]
         else:
+            # Redo digest auth from scratch to get a new nonce etc
             http = SmartHTTPConnection(si.host, si.port, si.ssl, si.afunix)
             try:
-                http.request("OPTIONS", self.getURI(si))
+                puri = list(urlparse.urlparse(self.getURI(si)))
+                puri[2] = urllib.quote(puri[2])
+                quri = urlparse.urlunparse(puri)
+                http.request("OPTIONS", quri)
 
                 response = http.getresponse()
 
@@ -262,6 +268,7 @@ class request(object):
                     for (k, v) in [p.split('=', 1) for p in parts]:
                         details[k.strip()] = unq(v.strip())
 
+                    details["max-nonce-time"] = time.time() + 600
                     self.manager.digestCache[user] = details
                     break
 
